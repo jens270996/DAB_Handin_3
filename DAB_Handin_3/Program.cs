5aeb822e796082289d71e17bd664887a1bcd480d
jens270996 <55832697+jens270996@users.noreply.github.com>
2020-12-07 10:10:13 +0100

    Insert citizen and tests

--- DAB_Handin_3/Program.cs
+++ DAB_Handin_3/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using DAB_Handin_3.Models;
 using DAB_Handin_3.Services;
+using System.Linq;
 
 namespace DAB_Handin_3
 {
@@ -10,12 +11,25 @@ namespace DAB_Handin_3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var service = new CitizenService(CovidDatabaseSettings.DatabaseSettings);
-            var citizens=service.Get();
+            var service = new CovidDbService(CovidDatabaseSettings.DatabaseSettings);
 
+
+            
+
+
+            var citizens=service.Get();
+            int id=citizens.First().ID;
+            service.AddTest(new Test { Date = DateTime.Now, Res = "pos", Status = "Fine" },id);
+            citizens = service.Get();
             foreach(var cit in citizens)
             {
+
                 Console.WriteLine($"Citizen:\n  Name: {cit.FirstName}, Muni: {cit.Muni}");
+                Console.WriteLine("Tests:");
+                foreach(var test in cit.Tests)
+                {
+                    Console.WriteLine($"Test result: {test.Res}, Test date: {test.Date}");
+                }
             }
         }
     }
